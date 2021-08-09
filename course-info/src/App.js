9ab1ff0a4f9e808955e16e99ee928140ed7b4a19
Karthik R <karthik@Karthiks-MacBook-Air.local>
2021-08-09 20:33:06 +0530

    exercise 2

--- course-info/src/App.js
+++ course-info/src/App.js
@@ -13,9 +13,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part} {props.exercise}
-      </p>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3}/>
     </div>
   )
 }
@@ -24,6 +24,16 @@ const Total = (props) => {
   return (
     <div>
       <p>Number of exercises {props.total}</p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
     </div>
   )
 }
@@ -40,9 +50,9 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content part1={part1} exercise1={exercises1} />
+      <Content part2={part2} exercise2={exercises2} />
+      <Content part3={part3} exercise3={exercises3} />
       <Total total = {exercises1 + exercises2 + exercises3} />
     </div>
   )
