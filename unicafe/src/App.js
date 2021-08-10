fbf2b220257a49d3dd7932c1441689fc74d7048c
Karthik R <karthik@Karthiks-MacBook-Air.local>
2021-08-10 15:08:41 +0530

    unicafe exercise 3

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,27 +1,39 @@
 import React, { useState } from "react";
 
+const Statistics = ({ good, neutral, bad, total }) => {
+  if (total)
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+
+        <p>Total feedbacks received is {(total = good + neutral + bad)}</p>
+
+        <p>Your average score is {(good - bad) / total}</p>
+        <p>Positive feedbacks received is {(good / total) * 100} %</p>
+      </div>
+    );
+  else return <p>Enter feedback about us</p>;
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
-  let total = good + neutral + bad;
-  let average = 0;
-  average = (good - bad) / total
   return (
     <div>
       <h1>Give Feedback</h1>
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
-      <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>Total feedbacks received is {total}</p>
-      
-      <p>Your average score is {average}</p>
-      <p>Positive feedbacks received is {(good / total) * 100} %</p>
-      
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={good + bad + neutral}
+      />
     </div>
   );
 };
